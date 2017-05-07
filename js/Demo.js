bc3c7aff4dffdd6ba4ca0b2117744d1f4cbeff7b
unknown <mandeepdhindsa21@gmail.com>
2017-05-07 04:11:02 -0400

    add my js function

---
+++ js/Demo.js
@@ -0,0 +1,11 @@
+function myFunction() {
+var x=document.getElementById("myemail").value;
+if (x != null) {
+/*  var y=  document.getElementById("demo").innerHTML ="Hello ";*/
+
+    document.getElementById("demo");
+     console.log("Thanks for signing up for our mailing list," +x);
+
+      /*alert("Thanks for signing up for our mailing list" +x);*/
+        }
+}
